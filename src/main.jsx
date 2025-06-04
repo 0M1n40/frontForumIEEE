6ab0510fb4ba64c85619ca60d2698af6a367cb33
Marco Antonio Lira Barros <malirab2302@gmail.com>
2025-06-03 22:44:21 -0300

    refactor: retirar rotas de admin

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import './index.css'
 
 import Home from './pages/Home'
 import Login from './pages/login/Login'
-import Register from './pages/Register'
 import ForgotPassword from './pages/ForgotPassword'
 import NotFound from './pages/NotFound'
 import Unauthorized from './pages/Unauthorized'
@@ -11,7 +10,6 @@ import Unauthorized from './pages/Unauthorized'
 import { AuthProvider } from './contexts/AuthContext'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import ProtectedRoute from './components/protectedRoute/ProtectedRoute'
 import Cadastro from './pages/cadastro/Cadastro'
 
 createRoot(document.getElementById('root')).render(
@@ -20,21 +18,12 @@ createRoot(document.getElementById('root')).render(
       <Routes>
         <Route 
           path="/" 
-          element={ 
-            <ProtectedRoute requiredRole="user">
+          element={
               <Home />
-            </ProtectedRoute>
           } />
         <Route path="/login" element={ <Login />} />
         <Route path="/cadastrar" element={ <Cadastro />} />
         <Route path="/forgot-password" element={ <ForgotPassword />} />
-        <Route 
-          path="/users" 
-          element={ 
-            <ProtectedRoute requiredRole="admin">
-              
-            </ProtectedRoute>
-          } />
         <Route path="/unauthorized" element={ <Unauthorized />} />
         <Route path="*" element={ <NotFound />} />
       </Routes>
